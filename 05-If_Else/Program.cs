18745462deb8bec5d121ac33277fe8c8ca80e8e6
Dmitry Yarmiychuk <yarmiychuk@gmail.com>
2022-07-08 01:02:01 +0300

    Hello, Masha

---
+++ 05-If_Else/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.Write("Введите имя польователя: ");
+String userName = Console.ReadLine();
+if (userName.ToLower == "маша") {
+    Console.WriteLine("Ура, это же МАША!");
+} else {
+    Console.WriteLine("Привет, " + userName);
+}
