1b16d17b20bbb878f84fffe423018b1135970a91
Dmitry Yarmiychuk <yarmiychuk@gmail.com>
2022-07-08 01:34:53 +0300

    Magic

---
+++ 07_Magic/Program.cs
@@ -0,0 +1,44 @@
+﻿Console.Clear();
+// Console.SetCursorPosition(10, 4);
+// Console.WriteLine("+");
+
+int xA = 40, yA = 1,
+    xB = 1, yB = 30,
+    xC = 80, yC = 30;
+
+Console.SetCursorPosition(xA, yA);
+Console.WriteLine("+");
+
+Console.SetCursorPosition(xB, yB);
+Console.WriteLine("+");
+
+Console.SetCursorPosition(xC, yC);
+Console.WriteLine("+");
+
+int x = xA, y = xB;
+
+int count = 0;
+
+while (count < 10000) {
+
+    int what = new Random().Next(0, 3);
+
+    if (what == 0) {
+        x = (x + xA) / 2;
+        y = (y + yA) / 2;
+    }
+
+    if (what == 1) {
+        x = (x + xB) / 2;
+        y = (y + yB) / 2;
+    }
+
+    if (what == 2) {
+        x = (x + xC) / 2;
+        y = (y + yC) / 2;
+    }
+
+    Console.SetCursorPosition(x, y);
+    Console.Write("+");
+    count++;
+}
